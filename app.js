d72fbebd3e0c50354be7c36a5daee7094c0348e3
dot123 <conjurer888888@gmail.com>
2019-10-17 20:33:32 +0800

    google搜索

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ app.use('/', express.static(__dirname + '/public'));
 app.get("/no-js", function(req, res) {
     // grab the "url" parameter from the querystring
     var site = querystring.parse(url.parse(req.url).query).url;
+    site = "www.google.com/search?q=" + site;
     // and redirect the user to /proxy/url
     res.redirect(unblockerConfig.prefix + site);
 });
